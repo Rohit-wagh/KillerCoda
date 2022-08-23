4844411f4a6864ed64151a6fc2516bc505798f68
Rohit Wagh <63150591+Rohit-wagh@users.noreply.github.com>
2022-08-23 16:43:42 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt update -y 
+sudo apt upgrade -y
+sudo apt install -y curl wget apt-transport-https
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
+minikube version
